0bf908ee77d356f70fe82dbb4cd96b1047ef1cb2
David Fudge <rkstar@mac.com>
2015-10-29 00:27:13 -0400

    update rkstar:spotify version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rkstar:accounts-spotify',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'Accounts service for Spotify',
   // URL to the Git repository containing the source code for this package.
@@ -17,7 +17,7 @@ Package.onUse(function(api) {
   api.imply('accounts-base')
 
   api.use('accounts-oauth')
-  api.use('rkstar:spotify@1.0.1')
+  api.use('rkstar:spotify@1.0.2')
 
   api.addFiles('common.js')
   api.addFiles('client.js', 'client')
